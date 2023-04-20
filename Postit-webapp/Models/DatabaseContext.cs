32413fb1a66bc7c12c57bb3c58a6348ef39785cc
Joao <Joao@DESKTOP-GTUHHR0>
2023-04-20 17:09:19 -0300

    Add project files.

---
+++ Postit-webapp/Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Postit_webapp.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Postit-webapp/Models/Note.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Postit_webapp.Models
+{
+    [Table("Note")]
+    public class Note
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Column("Description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Column("CreationDate")]
+        [Display(Name = "CreationDate")]
+        public DateTime CreationDate { get; set; }
+        [Column("IsFinished")]
+        [Display(Name = "IsFinished")]
+        public bool IsFinished { get; set; }
+        [Column("IsFavorite")]
+        [Display(Name = "IsFavorite")]
+        public bool IsFavorite{ get; set; }
+    }
+}
